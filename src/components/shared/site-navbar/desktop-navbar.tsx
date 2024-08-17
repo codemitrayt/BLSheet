878bdb3f39d995dcce4571f346a0a0ccb51f910d
codemitrayt <codemitrayt@gmail.com>
2024-08-17 11:23:17 +0530

    add desktop navbar

---
+++ src/components/shared/site-navbar/desktop-navbar.tsx
@@ -0,0 +1,9 @@
+const DesktopNavbar = () => {
+  return (
+    <div className="hidden sm:flex items-center justify-between w-[90%]">
+      DesktopNavbar
+    </div>
+  );
+};
+
+export default DesktopNavbar;
