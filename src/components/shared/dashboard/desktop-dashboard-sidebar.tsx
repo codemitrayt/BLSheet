85155317b5bb9048c7f8fe2c984e0a4af50daf65
codemitrayt <codemitrayt@gmail.com>
2024-08-17 18:40:53 +0530

    add dashboard routes

--- src/components/shared/dashboard/desktop-dashboard-sidebar.tsx
+++ src/components/shared/dashboard/desktop-dashboard-sidebar.tsx
@@ -1,32 +1,32 @@
 import { Link, NavLink } from "react-router-dom";
 
-import { GoHome } from "react-icons/go";
-
 import Logo from "../../ui/logo";
 import { cn } from "../../../utils";
+import dashboardLinks from "../../../configs/dashboard-links";
 
 const DesktopDashboardSidebar = () => {
   return (
     <div className="hidden md:block">
-      <div>
-        <Link to="/">
-          <Logo className="text-white" hasText={true} />
-        </Link>
-      </div>
+      <Link to="/">
+        <Logo className="text-white" hasText={true} />
+      </Link>
 
-      <div className="px-4 w-full flex items-center justify-center flex-col mt-8">
-        <NavLink
-          to="/dashboard/home"
-          className={({ isActive }) =>
-            cn(
-              "border border-gray-300/80 w-full flex items-center space-x-1 text-white hover:bg-white/80 hover:text-primary transition-all p-2 rounded-md",
-              isActive && "text-primary bg-white/80"
-            )
-          }
-        >
-          <GoHome className=" size-6" />
-          <span>Home</span>
-        </NavLink>
+      <div className="px-4 w-full flex space-y-6 items-center justify-center flex-col mt-8">
+        {dashboardLinks.map((link) => (
+          <NavLink
+            key={link.id}
+            to={link.path}
+            className={({ isActive }) =>
+              cn(
+                "border border-gray-300/80 w-full flex items-center space-x-1 text-white hover:bg-white/80 hover:text-primary transition-all p-2 rounded-md",
+                isActive && "text-primary bg-white/80"
+              )
+            }
+          >
+            <link.icon className="size-6" />
+            <span>{link.title}</span>
+          </NavLink>
+        ))}
       </div>
     </div>
   );
