0797a1bb113ae750a27e8faacad9a5399e8ba7a8
codemitrayt <codemitrayt@gmail.com>
2024-08-17 11:24:31 +0530

    add logo ui component

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,5 +1,22 @@
-const Logo = () => {
-  return <div>Logo</div>;
+import { BiSpreadsheet } from "react-icons/bi";
+
+import siteConfig from "../../configs/site-config";
+
+interface LogoProps {
+  hasText?: boolean;
+}
+
+const Logo = ({ hasText = false }: LogoProps) => {
+  return (
+    <div className="flex items-center justify-center space-x-1">
+      <BiSpreadsheet className="size-8 p-1 rounded-full text-primary" />
+      {hasText && (
+        <span className="text-pretty font-semibold text-lg text-primary">
+          {siteConfig.name}
+        </span>
+      )}
+    </div>
+  );
 };
 
 export default Logo;
