1c71c603bc8510e2186a5ad60607ba4dbf4425af
codemitrayt <codemitrayt@gmail.com>
2024-08-17 23:03:08 +0530

    add dashboard header

--- src/layouts/dashboard-layout.tsx
+++ src/layouts/dashboard-layout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import { Avatar, Input } from "antd";
 
 import DashboardSidebar from "../components/shared/dashboard/dashboard-sidebar";
 
@@ -6,7 +7,17 @@ const DashboardLayout = () => {
   return (
     <div className="min-h-screen flex">
       <DashboardSidebar />
-      <Outlet />
+      <div className="flex flex-col py-3 px-6 w-full">
+        <div className="h-12 flex items-center justify-center w-full space-x-6">
+          <Input.Search placeholder="Search activity" />
+          <div>
+            <Avatar className="bg-primary">R</Avatar>
+          </div>
+        </div>
+        <div className="h-[calc(100vh_-80px)] bg-white border rounded-lg overflow-auto p-5 shadow-sm">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 };
