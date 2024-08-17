955e07a6f206f463aeb2f6416dfa4d58c407c391
codemitrayt <codemitrayt@gmail.com>
2024-08-17 18:28:58 +0530

    change logo style

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,17 +1,24 @@
 import { BiSpreadsheet } from "react-icons/bi";
 
 import siteConfig from "../../configs/site-config";
+import { cn } from "../../utils";
 
 interface LogoProps {
   hasText?: boolean;
+  className?: string;
 }
 
-const Logo = ({ hasText = false }: LogoProps) => {
+const Logo = ({ hasText = false, className }: LogoProps) => {
   return (
-    <div className="flex items-center justify-center space-x-1">
-      <BiSpreadsheet className="size-8 p-1 rounded-full text-primary" />
+    <div
+      className={cn(
+        "flex items-center justify-center space-x-1 text-primary",
+        className
+      )}
+    >
+      <BiSpreadsheet className="size-8 p-1 rounded-full" />
       {hasText && (
-        <span className="text-pretty font-semibold text-lg text-primary">
+        <span className="text-pretty font-semibold text-lg">
           {siteConfig.name}
         </span>
       )}
