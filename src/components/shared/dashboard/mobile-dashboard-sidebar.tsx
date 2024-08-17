b1346d07c26469a7d87a990b37c1c8818beaa124
codemitrayt <codemitrayt@gmail.com>
2024-08-17 18:41:02 +0530

    add dashboard routes

--- src/components/shared/dashboard/mobile-dashboard-sidebar.tsx
+++ src/components/shared/dashboard/mobile-dashboard-sidebar.tsx
@@ -1,9 +1,8 @@
 import { Link, NavLink } from "react-router-dom";
 
-import { GoHome } from "react-icons/go";
-
 import Logo from "../../ui/logo";
 import { cn } from "../../../utils";
+import dashboardLinks from "../../../configs/dashboard-links";
 
 const MobilDashboardSidebar = () => {
   return (
@@ -12,18 +11,21 @@ const MobilDashboardSidebar = () => {
         <Logo className="text-white" />
       </Link>
 
-      <div className="flex items-center justify-center flex-col mt-8">
-        <NavLink
-          to="/dashboard/home"
-          className={({ isActive }) =>
-            cn(
-              "text-white hover:text-gray-300 transition p-2 rounded-md",
-              isActive && "text-primary bg-white/80 hover:text-primary"
-            )
-          }
-        >
-          <GoHome className=" size-6" />
-        </NavLink>
+      <div className="flex items-center justify-center flex-col mt-8 space-y-6">
+        {dashboardLinks.map((link) => (
+          <NavLink
+            key={link.id}
+            to={link.path}
+            className={({ isActive }) =>
+              cn(
+                "text-white hover:text-gray-300 transition p-2 rounded-md",
+                isActive && "text-primary bg-white/80 hover:text-primary"
+              )
+            }
+          >
+            <link.icon className="size-6" />
+          </NavLink>
+        ))}
       </div>
     </div>
   );
