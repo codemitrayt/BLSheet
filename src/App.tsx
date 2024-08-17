89b4acf3160341cb4de15f05777118bd1d926436
codemitrayt <codemitrayt@gmail.com>
2024-08-17 09:52:58 +0530

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import Router from "./routes";
+
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={Router} />;
 };
 
 export default App;
