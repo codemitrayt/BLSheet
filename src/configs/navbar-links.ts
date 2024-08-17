0abf243116a0f22fa3d37730b52d9df654217792
codemitrayt <codemitrayt@gmail.com>
2024-08-17 11:24:41 +0530

    add navbar links configuraiton

---
+++ src/configs/navbar-links.ts
@@ -0,0 +1,14 @@
+import { GoHome } from "react-icons/go";
+
+import { NavLinkType } from "../types";
+
+const navbarLinks: NavLinkType[] = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    icon: GoHome,
+  },
+];
+
+export default navbarLinks;
