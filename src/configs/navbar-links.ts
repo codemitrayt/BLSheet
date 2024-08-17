f81ebfe8eec3e826156c00fe2cf03e79be43d700
codemitrayt <codemitrayt@gmail.com>
2024-08-17 18:29:04 +0530

    add navbar links

--- src/configs/navbar-links.ts
+++ src/configs/navbar-links.ts
@@ -1,4 +1,6 @@
 import { GoHome } from "react-icons/go";
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineSpaceDashboard } from "react-icons/md";
 
 import { NavLinkType } from "../types";
 
@@ -8,6 +10,21 @@ const navbarLinks: NavLinkType[] = [
     title: "Home",
     path: "/",
     icon: GoHome,
+    onyForDesktop: true,
+  },
+  {
+    id: 2,
+    title: "Profile",
+    path: "/profile",
+    icon: CgProfile,
+    onyForDesktop: false,
+  },
+  {
+    id: 2,
+    title: "Dashboard",
+    path: "/dashboard/home",
+    icon: MdOutlineSpaceDashboard,
+    onyForDesktop: true,
   },
 ];
 
