f91a4591b651a27f89070c3fa12b265a377db844
codemitrayt <codemitrayt@gmail.com>
2024-08-17 18:29:30 +0530

    add dashboard routes and layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,16 +7,24 @@ import {
 import BaseLayout from "../layouts/base-layout";
 import AuthLayout from "../layouts/auth-layout";
 import ProtectedLayout from "../layouts/protected-layout";
+import DashboardLayout from "../layouts/dashboard-layout";
 
 import HomePage from "../pages/home-page";
+
 import SignUpPage from "../pages/auth/sign-up-page";
 import SignInPage from "../pages/auth/sign-in-page";
+
+import DashboardHomePage from "../pages/dashboard/dashboard-home-page";
 
 const Router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<BaseLayout />}>
       <Route element={<ProtectedLayout />}>
         <Route index element={<HomePage />} />
+      </Route>
+
+      <Route path="dashboard" element={<DashboardLayout />}>
+        <Route path="home" element={<DashboardHomePage />} />
       </Route>
 
       <Route path="auth" element={<AuthLayout />}>
