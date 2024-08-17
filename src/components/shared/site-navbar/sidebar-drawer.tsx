78832d8723342ceb0cb6770ccef36ea78ca87ca6
codemitrayt <codemitrayt@gmail.com>
2024-08-17 18:28:46 +0530

    add links in sidebar drawer

--- src/components/shared/site-navbar/sidebar-drawer.tsx
+++ src/components/shared/site-navbar/sidebar-drawer.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 import { FiMenu } from "react-icons/fi";
 import { LuChevronLeftCircle } from "react-icons/lu";
+import { AiOutlineLogout } from "react-icons/ai";
 
 import NavItem from "./nav-item";
 import siteConfig from "../../../configs/site-config";
@@ -25,10 +26,37 @@ const SidebarDrawer = () => {
         title={<span className="text-blue-500 text-lg">{siteConfig.name}</span>}
         closeIcon={<LuChevronLeftCircle className="size-6 text-blue-500" />}
       >
-        <div className="space-y-2">
-          {navbarLinks.map((link) => (
-            <NavItem link={link} onCloseDrawer={onCloseDrawer} key={link.id} />
-          ))}
+        <div className="flex items-center flex-col justify-between w-full h-full">
+          <div className="space-y-2 w-full">
+            {navbarLinks
+              .filter((link) => link.onyForDesktop)
+              .map((link) => (
+                <NavItem
+                  link={link}
+                  onCloseDrawer={onCloseDrawer}
+                  key={link.id}
+                />
+              ))}
+          </div>
+
+          <div className="space-y-2 w-full border-t pt-3">
+            {navbarLinks
+              .filter((link) => !link.onyForDesktop)
+              .map((link) => (
+                <NavItem
+                  link={link}
+                  onCloseDrawer={onCloseDrawer}
+                  key={link.id}
+                />
+              ))}
+
+            <button className="w-full px-3 py-2 rounded-lg group flex items-center space-x-2 hover:bg-gray-100 transition border border-gray-300/30">
+              <AiOutlineLogout className="size-5 group-hover:text-primary transition" />
+              <span className="text-[16px] group-hover:text-primary transition">
+                Logout
+              </span>
+            </button>
+          </div>
         </div>
       </Drawer>
     </div>
