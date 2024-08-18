3553984c503270f901b6f23488729dc2847c96bb
codemitrayt <codemitrayt@gmail.com>
2024-08-18 19:24:56 +0530

    add style

--- src/pages/dashboard/sheet/index.tsx
+++ src/pages/dashboard/sheet/index.tsx
@@ -1,12 +1,13 @@
 import { Input } from "antd";
+
 import CreateSheet from "./helpers/create";
 import Show from "./helpers/show";
 
 const DashboardSheetPage = () => {
   return (
     <div className="relative">
-      <div className="flex items-center justify-center space-x-4">
-        <Input.Search placeholder="search sheet" />
+      <div className="flex items-center justify-between space-x-4">
+        <Input.Search placeholder="search sheet" className="max-w-sm" />
         <CreateSheet />
       </div>
       <div className="mt-6">
