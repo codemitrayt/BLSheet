bf93f949e1306cfdc45eeb1daa7028f3d8cde773
codemitrayt <codemitrayt@gmail.com>
2024-08-18 19:25:12 +0530

    add create sheet tab

--- src/pages/dashboard/sheet/helpers/create.tsx
+++ src/pages/dashboard/sheet/helpers/create.tsx
@@ -1,13 +1,52 @@
-import { Button, Drawer } from "antd";
+import { Button, Drawer, Tabs, TabsProps } from "antd";
 import { useState } from "react";
 
 import { IoMdAddCircleOutline } from "react-icons/io";
 import { LuChevronRightCircle } from "react-icons/lu";
+import { FaArrowTrendUp } from "react-icons/fa6";
+import { GoArrowDownLeft, GoArrowUpRight } from "react-icons/go";
+
+import IncomeTab from "./tabs/income-tab";
+import InvestmentTab from "./tabs/investment-tab";
+import ExpenseTab from "./tabs/expense-tab";
 
 const CreateSheet = () => {
   const [drawerState, setDrawerState] = useState(false);
   const onCloseDrawer = () => setDrawerState(false);
   const onOpenDrawer = () => setDrawerState(true);
+
+  const items: TabsProps["items"] = [
+    {
+      key: "income",
+      label: (
+        <span className="flex items-center justify-center space-x-2 px-2">
+          <GoArrowUpRight />
+          <span>Income</span>
+        </span>
+      ),
+      children: <IncomeTab />,
+    },
+    {
+      key: "expense",
+      label: (
+        <span className="flex items-center justify-center space-x-2 px-2">
+          <GoArrowDownLeft />
+          <span>Expense</span>
+        </span>
+      ),
+      children: <ExpenseTab />,
+    },
+    {
+      key: "investment",
+      label: (
+        <span className="flex items-center justify-center space-x-2 px-2">
+          <FaArrowTrendUp />
+          <span>Investment</span>
+        </span>
+      ),
+      children: <InvestmentTab />,
+    },
+  ];
 
   return (
     <div className="relative">
@@ -20,13 +59,13 @@ const CreateSheet = () => {
         Sheet
       </Button>
       <Drawer
-        width={600}
+        width={450}
         closeIcon={<LuChevronRightCircle className="text-primary size-5" />}
         open={drawerState}
         onClose={onCloseDrawer}
         title={<span className="text-primary">Create Sheet</span>}
       >
-        <h1>Hi</h1>
+        <Tabs items={items} />
       </Drawer>
     </div>
   );
