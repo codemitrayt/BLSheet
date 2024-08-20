5e52994f3012b5780a02fd38088ebe2ee3348e9f
codemitrayt <codemitrayt@gmail.com>
2024-08-20 15:52:58 +0530

    fix dashboard style

--- src/components/shared/dashboard/dashboard-sidebar.tsx
+++ src/components/shared/dashboard/dashboard-sidebar.tsx
@@ -3,8 +3,8 @@ import MobilDashboardSidebar from "./mobile-dashboard-sidebar";
 
 const DashboardSidebar = () => {
   return (
-    <div className="w-[130px] lg:w-[300px] py-3 pl-3">
-      <div className="bg-primary h-full py-8 rounded-3xl">
+    <div className="w-[100px] lg:w-[280px] py-8 pl-3">
+      <div className="bg-primary h-full py-8 rounded-3xl w-full">
         <MobilDashboardSidebar />
         <DesktopDashboardSidebar />
       </div>
