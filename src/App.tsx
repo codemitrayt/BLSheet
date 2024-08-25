4a702bdb758ad0c0ed0cdc85296b83fcb997eeeb
codemitrayt <codemitrayt@gmail.com>
2024-08-25 15:37:14 +0530

    add query client provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import { RouterProvider } from "react-router-dom";
 import { ConfigProvider } from "antd";
 
 import Router from "./routes";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const App = () => {
+  const queryClient = new QueryClient();
+
   return (
     <ConfigProvider
       theme={{
         token: { colorPrimary: "#2F667F" },
       }}
     >
-      <RouterProvider router={Router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={Router} />
+      </QueryClientProvider>
     </ConfigProvider>
   );
 };
