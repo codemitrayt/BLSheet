d2e6b6ae8c599c67cb0db14234e44d4f8c184df1
codemitrayt <codemitrayt@gmail.com>
2024-08-26 08:29:50 +0530

    add useUserInfo hook

--- src/hooks/useUserInfo.ts
+++ src/hooks/useUserInfo.ts
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
+
 const useUserInfo = () => {
-  return { user: {}, authToken: "" };
+  const { authToken, user, isAuth } = useSelector(
+    (state: RootState) => state.auth
+  );
+  return { user, authToken, isAuth };
 };
 
 export default useUserInfo;
