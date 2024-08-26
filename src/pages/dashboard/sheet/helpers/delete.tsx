63d69449f8172ab09a5415bb3c89df170692b19e
codemitrayt <codemitrayt@gmail.com>
2024-08-26 17:59:36 +0530

    add delete bl sheet component

--- src/pages/dashboard/sheet/helpers/delete.tsx
+++ src/pages/dashboard/sheet/helpers/delete.tsx
@@ -1,5 +1,52 @@
-const Delete = () => {
-  return <div>Delete</div>;
+import { useMutation } from "react-query";
+import blSheetService from "../../../../services/bl-sheet-service";
+import useUserInfo from "../../../../hooks/useUserInfo";
+import { TbTrash } from "react-icons/tb";
+import { RiLoader4Line } from "react-icons/ri";
+import ConfirmationPopUp from "../../../../components/ui/confirmation-popup";
+
+interface DeleteProps {
+  objectId: string;
+  refetchBLSheets: () => void;
+}
+
+const Delete = ({ objectId, refetchBLSheets }: DeleteProps) => {
+  const { authToken } = useUserInfo();
+
+  const { isLoading, mutate } = useMutation({
+    mutationKey: ["delete"],
+    mutationFn: ({ data }: { data: { objectId: string } }) =>
+      blSheetService().deleteBLSheet({ data, authToken }),
+    onSuccess: () => {
+      refetchBLSheets();
+    },
+    onError: (error) => {
+      console.error("ERROR :: delete bl sheet ::", error);
+    },
+    retry: false,
+  });
+
+  const handleDelete = () => {
+    mutate({ data: { objectId } });
+  };
+
+  return (
+    <ConfirmationPopUp
+      title="Delete BL Sheet"
+      description="Are you sure to delete this sheet?"
+      fn={handleDelete}
+      icon={
+        <div className="flex items-center justify-center h-5 mr-2">
+          <TbTrash className="text-red-500" />
+        </div>
+      }
+      isLoading={isLoading}
+    >
+      <button className="text-red-500 hover:text-red-500/80 transition">
+        {isLoading ? <RiLoader4Line className="animate-spin" /> : <TbTrash />}
+      </button>
+    </ConfirmationPopUp>
+  );
 };
 
 export default Delete;
