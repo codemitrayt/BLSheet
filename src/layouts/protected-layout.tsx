392a92a1c034f3cd529d6d0b50169d9e5a3d8616
codemitrayt <codemitrayt@gmail.com>
2024-08-26 08:30:15 +0530

    add authentication in layouts

--- src/layouts/protected-layout.tsx
+++ src/layouts/protected-layout.tsx
@@ -1,9 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 import SiteNavbar from "../components/shared/site-navbar";
 import SiteFooter from "../components/shared/site-footer";
+import useUserInfo from "../hooks/useUserInfo";
+import { URLS } from "../constants";
 
 const ProtectedLayout = () => {
+  const { isAuth } = useUserInfo();
+  if (!isAuth) return <Navigate to={URLS.signInPageUrl} />;
+
   return (
     <div className="flex flex-col min-h-screen">
       <SiteNavbar />
