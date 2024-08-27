daca77fd81d16b5752bdeafd7d0f7ca7d042564d
codemitrayt <codemitrayt@gmail.com>
2024-08-27 12:43:12 +0530

    add edit sheet feature

--- src/pages/dashboard/sheet/helpers/edit.tsx
+++ src/pages/dashboard/sheet/helpers/edit.tsx
@@ -1,5 +1,46 @@
-const Edit = () => {
-  return <div>Edit</div>;
+import { TbEdit } from "react-icons/tb";
+import { useState } from "react";
+import { Drawer } from "antd";
+
+import { LuChevronRightCircle } from "react-icons/lu";
+
+import EditSheetForm from "./edit-sheet-form";
+import { BLSheet } from "../../../../types";
+
+interface EditProps {
+  sheet: BLSheet;
+  refetchBLSheets: () => void;
+}
+
+const Edit = ({ sheet, refetchBLSheets }: EditProps) => {
+  const [drawerState, setDrawerState] = useState(false);
+  const onCloseDrawer = () => setDrawerState(false);
+  const onOpenDrawer = () => setDrawerState(true);
+
+  return (
+    <div>
+      <button
+        onClick={onOpenDrawer}
+        className="text-emerald-500 flex items-center justify-center hover:text-emerald-500/80 transition"
+      >
+        <TbEdit />
+      </button>
+
+      <Drawer
+        width={450}
+        closeIcon={<LuChevronRightCircle className="text-primary size-5" />}
+        open={drawerState}
+        onClose={onCloseDrawer}
+        title={<span className="text-primary">Edit Sheet</span>}
+      >
+        <EditSheetForm
+          sheet={sheet}
+          refetchBLSheet={refetchBLSheets}
+          onCloseDrawer={onCloseDrawer}
+        />
+      </Drawer>
+    </div>
+  );
 };
 
 export default Edit;
