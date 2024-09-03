f2abe658969878dbf04b396d84486a46693f28a5
mungsehari <mungse.hari@gmail.com>
2024-09-04 00:06:21 +0530

    create delete page

--- src/pages/dashboard/todo/helpers/delete.tsx
+++ src/pages/dashboard/todo/helpers/delete.tsx
@@ -1,5 +1,58 @@
-const DeleteTodo = () => {
-  return <div>DeleteTodo</div>;
+import { useMutation } from "react-query";
+import useUserInfo from "../../../../hooks/useUserInfo";
+import { Todo } from "../../../../types";
+import todoService from "../../../../services/todo-service";
+import ConfirmationPopUp from "../../../../components/ui/confirmation-popup";
+import { TbTrash } from "react-icons/tb";
+import { Tooltip } from "antd";
+import { RiLoader4Line } from "react-icons/ri";
+
+interface TodoCardProps {
+  objectId: string;
+  refetchTodoList: () => {};
+}
+const DeleteTodo = ({ objectId, refetchTodoList }: TodoCardProps) => {
+  const { authToken } = useUserInfo();
+  const { isLoading, mutate } = useMutation({
+    mutationKey: ["delete"],
+    mutationFn: ({ data }: { data: { objectId: string } }) =>
+      todoService().deleteTodo({ data, authToken }),
+    onSuccess: () => {
+      refetchTodoList();
+    },
+    onError: (error) => {
+      console.error("ERROR :: delete Todo ::", error);
+    },
+    retry: false,
+  });
+  const handleDelete = () => {
+    mutate({ data: { objectId } });
+  };
+  return (
+    <>
+      <ConfirmationPopUp
+        title="Delete BL Sheet"
+        description="Are you sure to delete this sheet?"
+        fn={handleDelete}
+        icon={
+          <div className="flex items-center justify-center h-5 mr-2">
+            <TbTrash className="text-red-500" />
+          </div>
+        }
+        isLoading={isLoading}
+      >
+        <Tooltip title="Delete sheet">
+          <button className="text-red-500 hover:text-red-500/80 transition">
+            {isLoading ? (
+              <RiLoader4Line className="animate-spin" />
+            ) : (
+              <TbTrash />
+            )}
+          </button>
+        </Tooltip>
+      </ConfirmationPopUp>
+    </>
+  );
 };
 
 export default DeleteTodo;
