ff1badab76188b8d8116a91d9d6c1a2324ce5292
codemitrayt <codemitrayt@gmail.com>
2024-09-03 23:16:20 +0530

    add show todo component

--- src/pages/dashboard/todo/helpers/show.tsx
+++ src/pages/dashboard/todo/helpers/show.tsx
@@ -1,5 +1,19 @@
-const ShowTodo = () => {
-  return <div>ShowTodo</div>;
+import { Todo } from "../../../../types";
+import TodoCard from "../cards/todo-card";
+
+interface ShowTodoProps {
+  todoList: Todo[];
+  refetchTodoList: () => {};
+}
+
+const ShowTodoList = ({ todoList, refetchTodoList }: ShowTodoProps) => {
+  return (
+    <div className="grid grid-cols-4 gap-3">
+      {todoList.map((todo) => (
+        <TodoCard todo={todo} refetchTodoList={refetchTodoList} />
+      ))}
+    </div>
+  );
 };
 
-export default ShowTodo;
+export default ShowTodoList;
