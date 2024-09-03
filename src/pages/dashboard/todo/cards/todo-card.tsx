62cda6f0ad06bcb484b18a2d81a334b5b3eaf634
codemitrayt <codemitrayt@gmail.com>
2024-09-03 23:16:10 +0530

    add todo card

--- src/pages/dashboard/todo/cards/todo-card.tsx
+++ src/pages/dashboard/todo/cards/todo-card.tsx
@@ -1,36 +1,32 @@
-import { IoIosGitMerge } from "react-icons/io";
-import { Avatar, Tag } from "antd";
+import { Tag } from "antd";
+import { RiProgress1Line } from "react-icons/ri";
 
-const TodoCard = () => {
+import { Todo } from "../../../../types";
+
+interface TodoCardProps {
+  todo: Todo;
+  refetchTodoList: () => {};
+}
+
+const TodoCard = ({ todo }: TodoCardProps) => {
   return (
     <div className="bg-card p-3 space-y-2 rounded-lg shadow-lg transition-transform transform border hover:bg-gray-100 cursor-pointer">
-      <h3 className="flex gap-2 items-center">Hold introductory meeting</h3>
-
-      <p className="text-gray-700 pb-2 text-sm">
-        Discuss the project progress, potential challenges, and solutions.
+      <div className="flex items-center space-x-1">
+        <RiProgress1Line className="text-primary size-4" />
+        <h3 className="flex gap-2 items-center text-primary font-medium">
+          {todo.title}
+        </h3>
+      </div>
+      <p className="text-gray-700 pb-2 text-sm font-light">
+        {todo.description}
       </p>
 
-      <div className="flex space-x-2">
-        <Tag color="green">Medium</Tag>
-        <Tag color="red">In Progress</Tag>
+      <div className="flex">
+        <Tag color="green">{todo.level}</Tag>
+        <Tag color="red">{todo.status}</Tag>
       </div>
 
-      <div className=" flex items-center justify-between mt-6 ">
-        <span className=" flex items-center gap-2 text-muted-foreground">
-          <Avatar
-            style={{
-              backgroundColor: "#fde3cf",
-              color: "#f56a00",
-            }}
-          >
-            U
-          </Avatar>{" "}
-          <span>12-14 Jul</span>
-        </span>
-        <p className="mt-1 text-muted-foreground flex items-center gap-1">
-          2 <IoIosGitMerge />
-        </p>
-      </div>
+      <div className="flex items-center justify-end">Delete</div>
     </div>
   );
 };
