971050b6cd0ff42d81dedad8f9dad16d9c0ed5c2
codemitrayt <codemitrayt@gmail.com>
2024-09-03 23:16:02 +0530

    add display todo

--- src/pages/dashboard/todo/index.tsx
+++ src/pages/dashboard/todo/index.tsx
@@ -1,11 +1,41 @@
-import TodoCard from "./cards/todo-card";
+import { useQuery } from "react-query";
+import { useState } from "react";
+import { Spin } from "antd";
+
+import useUserInfo from "../../../hooks/useUserInfo";
+import useErrorHandler from "../../../hooks/useErrorHandler";
+import todoService from "../../../services/todo-service";
+
+import ShowTodoList from "./helpers/show";
+import { Todo } from "../../../types";
 
 const DashboardTodoPage = () => {
-  return (
-    <div className="grid grid-cols-4">
-      <TodoCard />
-    </div>
-  );
+  const { authToken } = useUserInfo();
+  const { handleError } = useErrorHandler();
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  const { refetch: refetchTodoList, isLoading } = useQuery({
+    queryKey: ["todo-list"],
+    queryFn: () => todoService().getTodoList({ authToken }),
+    onSuccess: ({ data }) => {
+      const todoList = data?.message?.todoList || [];
+      setTodoList(todoList);
+    },
+    onError: (error) => {
+      console.log("ERROR :: todo-list ::", error);
+      handleError(error);
+    },
+    retry: false,
+  });
+
+  if (isLoading)
+    return (
+      <div className="py-16 flex items-center justify-center">
+        <Spin />
+      </div>
+    );
+
+  return <ShowTodoList todoList={todoList} refetchTodoList={refetchTodoList} />;
 };
 
 export default DashboardTodoPage;
