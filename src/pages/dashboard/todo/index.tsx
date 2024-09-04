10aab07bced3bdb5e3d5a8cf4ce351c5d5c7b142
codemitrayt <codemitrayt@gmail.com>
2024-09-04 19:32:42 +0530

    add cards status wise

--- src/pages/dashboard/todo/index.tsx
+++ src/pages/dashboard/todo/index.tsx
@@ -34,7 +34,42 @@ const DashboardTodoPage = () => {
       </div>
     );
 
-  return <ShowTodoList todoList={todoList} refetchTodoList={refetchTodoList} />;
+  return (
+    <div className="relative h-full border bg-white rounded-lg shadow-sm">
+      <div className="grid grid-cols-3 gap-4 text-primary bg-gray-100 rounded-tr-lg rounded-tl-lg overflow-hidden p-6">
+        <h1 className="border border-primary p-1 w-full text-center">
+          Pending
+        </h1>
+        <h1 className="border border-primary p-1 w-full text-center">
+          In Progress
+        </h1>
+        <h1 className="border border-primary p-1 w-full text-center">
+          Completed
+        </h1>
+      </div>
+
+      <div className="flex justify-between h-[calc(100vh_-200px)] gap-4 m-6">
+        <div className="flex flex-col space-y-3 w-full overflow-y-auto h-full">
+          <ShowTodoList
+            todoList={todoList.filter((todo) => todo.status === "pending")}
+            refetchTodoList={refetchTodoList}
+          />
+        </div>
+        <div className="flex flex-col space-y-3 w-full overflow-y-auto h-full">
+          <ShowTodoList
+            todoList={todoList.filter((todo) => todo.status === "in_progress")}
+            refetchTodoList={refetchTodoList}
+          />
+        </div>
+        <div className="flex flex-col space-y-3 w-full overflow-y-auto h-full">
+          <ShowTodoList
+            todoList={todoList.filter((todo) => todo.status === "completed")}
+            refetchTodoList={refetchTodoList}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardTodoPage;
