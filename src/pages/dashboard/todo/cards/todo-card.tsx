d89fe82d75d424654d215cacd409155b2a07c3a4
codemitrayt <codemitrayt@gmail.com>
2024-09-05 18:23:49 +0530

    refactor todo card

--- src/pages/dashboard/todo/cards/todo-card.tsx
+++ src/pages/dashboard/todo/cards/todo-card.tsx
@@ -2,13 +2,14 @@ import { Tag } from "antd";
 import { RiProgress1Line } from "react-icons/ri";
 
 import { Todo } from "../../../../types";
+import UpdateTodo from "../helpers/update";
 
 interface TodoCardProps {
   todo: Todo;
   refetchTodoList: () => {};
 }
 
-const TodoCard = ({ todo }: TodoCardProps) => {
+const TodoCard = ({ todo, refetchTodoList }: TodoCardProps) => {
   return (
     <div className="bg-card p-3 space-y-2 rounded-lg shadow-lg transition-transform transform border hover:bg-gray-100 cursor-pointer">
       <div className="flex items-center space-x-1">
@@ -26,7 +27,9 @@ const TodoCard = ({ todo }: TodoCardProps) => {
         <Tag color="red">{todo.status}</Tag>
       </div>
 
-      <div className="flex items-center justify-end">Delete</div>
+      <div className="flex items-center justify-end">
+        <UpdateTodo todo={todo} refetchTodoList={refetchTodoList} />
+      </div>
     </div>
   );
 };
