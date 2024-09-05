5d5c12a6fa8d03aee034701db2a25c5f998f5fb9
codemitrayt <codemitrayt@gmail.com>
2024-09-05 18:23:41 +0530

    change constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -17,14 +17,14 @@ export const BL_SHEET_TYPES = [
   { label: "Investment", value: "investment" },
 ];
 
-export const TODO_CARD_LEVEL_TYPES = [
-  { label: "Low", value: "low" },
+export const TODO_LEVELS = [
+  { label: "Easy", value: "easy" },
   { label: "Medium", value: "medium" },
   { label: "Hard", value: "hard" },
 ];
 
-export const TODO_CARD_STATUS_TYPES = [
-  { label: "In Progress", value: "inprogress" },
-  { label: "On Track", value: "ontrack" },
-  { label: "Off Track", value: "offtrack" },
+export const TODO_STATUS = [
+  { label: "In Progress", value: "in_progress" },
+  { label: "Pending", value: "pending" },
+  { label: "Completed", value: "completed" },
 ];
