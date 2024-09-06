0d8d4dd1120f3b6ad9ebfdd16034330b28a2b204
codemitrayt <codemitrayt@gmail.com>
2024-09-06 14:26:13 +0530

    added style

--- src/constants/icon.tsx
+++ src/constants/icon.tsx
@@ -3,7 +3,7 @@ import { MdPending } from "react-icons/md";
 import { RiProgress3Fill } from "react-icons/ri";
 
 export const TODO_STATUS_ICON = {
-  pending: <MdPending className="text-black" />,
-  in_progress: <RiProgress3Fill className="text-black" />,
-  completed: <IoIosCheckmarkCircle className="text-black" />,
+  pending: <MdPending className="text-primary lg:text-black" />,
+  in_progress: <RiProgress3Fill className="text-primary lg:text-black" />,
+  completed: <IoIosCheckmarkCircle className="text-primary lg:text-black" />,
 };
