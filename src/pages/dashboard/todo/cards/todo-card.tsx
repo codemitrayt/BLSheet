79a45c9dd61928c682b2d13f3f11bc7e7459800a
codemitrayt <codemitrayt@gmail.com>
2024-09-06 14:27:03 +0530

    remove icon

--- src/pages/dashboard/todo/cards/todo-card.tsx
+++ src/pages/dashboard/todo/cards/todo-card.tsx
@@ -8,7 +8,6 @@ import { Todo } from "../../../../types";
 
 import { strSlice } from "../../../../utils";
 import { TODO_LEVEL_COLOR, TODO_STATUS_COLOR } from "../../../../constants";
-import { TODO_STATUS_ICON } from "../../../../constants/icon";
 
 interface TodoCardProps {
   todo: Todo;
@@ -19,13 +18,10 @@ const TodoCard = ({ todo, refetchTodoList }: TodoCardProps) => {
   return (
     <div className="bg-card p-3 space-y-2 rounded-lg shadow-lg transition-transform transform border hover:bg-gray-100 cursor-pointer">
       <div className="flex items-center space-x-1">
-        {TODO_STATUS_ICON[todo.status]}
-        <h3 className="flex gap-2 items-center text-black font-medium">
-          {strSlice(todo.title, 35)}
-        </h3>
+        <h3 className="text-black font-medium">{strSlice(todo.title, 30)}</h3>
       </div>
-      <p className="text-gray-700 pb-2 text-sm font-light h-[40px]">
-        {strSlice(todo.description, 120)}
+      <p className="text-gray-700 pb-2 text-sm font-light h-[45px]">
+        {strSlice(todo.description, 65)}
       </p>
 
       <div className="flex">
