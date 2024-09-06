63f9ec2a2b3ea51408a396035b3600840fce5b71
codemitrayt <codemitrayt@gmail.com>
2024-09-06 09:49:52 +0530

    add keyframes and animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,21 @@ export default {
         secondary: "#539BBB",
         turnary: "#EEF5F8",
       },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        shimmer: "shimmer 2s linear infinite",
+      },
+    },
+    keyframes: {
+      shimmer: {
+        from: {
+          backgroundPosition: "0 0",
+        },
+        to: {
+          backgroundPosition: "-200% 0",
+        },
+      },
     },
   },
   plugins: [],
