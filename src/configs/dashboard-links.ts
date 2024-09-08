6efc38a22c65fa4085152385fc057f36c7424c60
codemitrayt <codemitrayt@gmail.com>
2024-09-08 17:12:42 +0530

    add projects and task links

--- src/configs/dashboard-links.ts
+++ src/configs/dashboard-links.ts
@@ -1,7 +1,9 @@
+import { GoProject } from "react-icons/go";
 import { NavLinkType } from "../types";
 
 import { HiHome, HiTable } from "react-icons/hi";
 import { LuListTodo } from "react-icons/lu";
+import { MdOutlineTaskAlt } from "react-icons/md";
 
 const dashboardLinks: NavLinkType[] = [
   {
@@ -12,11 +14,13 @@ const dashboardLinks: NavLinkType[] = [
   },
   {
     id: 2,
-    title: "Sheet",
+    title: "Sheets",
     path: "/dashboard/sheet",
     icon: HiTable,
   },
-  { id: 3, title: "Todo", path: "/dashboard/todo", icon: LuListTodo },
+  { id: 3, title: "Todos", path: "/dashboard/todo", icon: LuListTodo },
+  { id: 3, title: "Projects", path: "/dashboard/projects", icon: GoProject },
+  { id: 3, title: "Tasks", path: "/dashboard/tasks", icon: MdOutlineTaskAlt },
 ];
 
 export default dashboardLinks;
