8e5f1db792304d02774fc073228e98117f58c04d
codemitrayt <codemitrayt@gmail.com>
2024-09-10 16:30:49 +0530

    build(comp): show project ui component

--- src/pages/dashboard/project/helpers/show.tsx
+++ src/pages/dashboard/project/helpers/show.tsx
@@ -1,5 +1,23 @@
-const ShowProjects = () => {
-  return <div>ShowProjects</div>;
+import ProjectCard from "../cards/project-card";
+import { Project } from "../../../../types";
+
+interface ShowProjectsProps {
+  projects: Project[];
+  refetchProjectList: () => void;
+}
+
+const ShowProjects = ({ projects, refetchProjectList }: ShowProjectsProps) => {
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {projects.map((project) => (
+        <ProjectCard
+          project={project}
+          key={project._id}
+          refetchProjectList={refetchProjectList}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default ShowProjects;
