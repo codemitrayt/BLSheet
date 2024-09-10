7f2d02cca30711f2d8950436046f94271f4be59e
codemitrayt <codemitrayt@gmail.com>
2024-09-10 16:30:21 +0530

    add project component

--- src/pages/dashboard/project/index.tsx
+++ src/pages/dashboard/project/index.tsx
@@ -1,5 +1,49 @@
+import { useQuery } from "react-query";
+import { useState } from "react";
+import { Spin } from "antd";
+
+import projectService from "../../../services/project-service";
+import useUserInfo from "../../../hooks/useUserInfo";
+import useErrorHandler from "../../../hooks/useErrorHandler";
+import { Project } from "../../../types";
+import ShowProjects from "./helpers/show";
+
 const DashboardProjectPage = () => {
-  return <div>Comming soon this feature...</div>;
+  const { authToken } = useUserInfo();
+  const { handleError } = useErrorHandler();
+  const [projectList, setProjectList] = useState<Project[]>([]);
+
+  const { isLoading, refetch: refetchProjectList } = useQuery({
+    queryKey: ["get-project-list"],
+    queryFn: () => projectService().getProjectList({ authToken }),
+    onSuccess: ({ data }) => {
+      const projects = data?.message?.projects || [];
+      setProjectList(projects);
+      console.log(projectList);
+    },
+    onError: (error) => {
+      console.error("ERROR :: get project list ::", error);
+      handleError(error);
+    },
+    retry: false,
+  });
+
+  if (isLoading) {
+    return (
+      <div className="py-16 flex items-center justify-center">
+        <Spin />
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-6 bg-white border rounded-xl shadow-sm h-[calc(100vh_-80px)] overflow-y-auto">
+      <ShowProjects
+        projects={projectList}
+        refetchProjectList={refetchProjectList}
+      />
+    </div>
+  );
 };
 
 export default DashboardProjectPage;
