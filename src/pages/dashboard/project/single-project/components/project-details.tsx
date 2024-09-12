41abbf3ff344f1e266a9cbe1289a5ca6aec7ad83
codemitrayt <codemitrayt@gmail.com>
2024-09-12 14:33:58 +0530

    add avatar group

--- src/pages/dashboard/project/single-project/components/project-details.tsx
+++ src/pages/dashboard/project/single-project/components/project-details.tsx
@@ -1,5 +1,6 @@
 import { Avatar, Button } from "antd";
 import { Project } from "../../../../../types";
+import { VscAdd } from "react-icons/vsc";
 
 interface ProjectDetailsProps {
   project: Project;
@@ -7,22 +8,23 @@ interface ProjectDetailsProps {
 
 const ProjectDetails = ({ project }: ProjectDetailsProps) => {
   return (
-    <div className="">
+    <div className="py-3">
       <div className="flex items-center justify-between w-full">
         <h1 className="text-xl font-medium text-primary"> {project.name}</h1>
         <div className="flex items-center space-x-3">
           <Avatar.Group
             max={{
-              count: 2,
+              count: 3,
               style: { background: "#2F667F" },
             }}
           >
             <Avatar style={{ background: "#2F667F" }}>R</Avatar>
             <Avatar style={{ background: "#2F667F" }}>P</Avatar>
+            <Avatar style={{ background: "#2F667F" }}>T</Avatar>
             <Avatar style={{ background: "#2F667F" }}>J</Avatar>
           </Avatar.Group>
 
-          <Button>Invite</Button>
+          <Button icon={<VscAdd />}>Invite</Button>
         </div>
       </div>
 
