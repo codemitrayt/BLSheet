f32402ac094a0bec06cd6705a13bd861a02a4251
codemitrayt <codemitrayt@gmail.com>
2024-09-13 23:45:09 +0530

    add view

--- src/pages/dashboard/project/index.tsx
+++ src/pages/dashboard/project/index.tsx
@@ -8,6 +8,8 @@ import useErrorHandler from "../../../hooks/useErrorHandler";
 import { Project } from "../../../types";
 import ShowProjects from "./helpers/show";
 import CreateProject from "./helpers/create";
+import { MdOutlineListAlt } from "react-icons/md";
+import { IoMdGrid } from "react-icons/io";
 
 const DashboardProjectPage = () => {
   const { authToken } = useUserInfo();
@@ -38,7 +40,12 @@ const DashboardProjectPage = () => {
 
   return (
     <div className="p-6">
-      <div className="flex items-center justify-end p-3 bg-gray-100 rounded-lg mb-4 border shadow-sm">
+      <div className="flex items-center justify-between p-3 bg-gray-100 rounded-lg mb-4 border shadow-sm">
+        <div className="space-x-3 flex items-center justify-center">
+          <span className="text-primary font-medium">View</span>
+          <IoMdGrid className="text-primary size-5 cursor-pointer" />
+          <MdOutlineListAlt className="text-black size-5 cursor-pointer" />
+        </div>
         <CreateProject refetchProjectList={refetchProjectList} />
       </div>
       <div className="h-[calc(100vh_-170px)] overflow-y-auto md:p-6 md:border md:rounded-lg md:shadow-sm scroll-smooth">
