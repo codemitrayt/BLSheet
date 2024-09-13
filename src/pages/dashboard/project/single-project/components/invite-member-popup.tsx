932e2adad8f07b5a2fbc0eb9ac8717d191b3f24b
codemitrayt <codemitrayt@gmail.com>
2024-09-13 23:23:20 +0530

    add invite member api

--- src/pages/dashboard/project/single-project/components/invite-member-popup.tsx
+++ src/pages/dashboard/project/single-project/components/invite-member-popup.tsx
@@ -2,19 +2,44 @@ import { Modal, Button, Input, Form } from "antd";
 import { useState } from "react";
 
 import { VscAdd } from "react-icons/vsc";
+import { useMutation } from "react-query";
+import projectService from "../../../../../services/project-service";
+import useUserInfo from "../../../../../hooks/useUserInfo";
+import useErrorHandler from "../../../../../hooks/useErrorHandler";
 
 interface InviteMemberPopupProps {
   projectName: string;
+  projectId: string;
 }
 
-const InviteMemberPopup = ({ projectName }: InviteMemberPopupProps) => {
+const InviteMemberPopup = ({
+  projectName,
+  projectId,
+}: InviteMemberPopupProps) => {
+  const { authToken } = useUserInfo();
+  const { handleError } = useErrorHandler();
+
   const [modalState, setModalState] = useState(false);
   const closeModal = () => setModalState(false);
   const openModal = () => setModalState(true);
 
+  const { mutate, isLoading } = useMutation({
+    mutationKey: ["invite-member"],
+    mutationFn: ({ data }: { data: { email: string; projectId: string } }) =>
+      projectService().inviteTeamMember({ data, authToken }),
+    onSuccess: () => {
+      closeModal();
+    },
+    onError: (error) => {
+      console.log("ERROR :: invite members ::", error);
+      handleError(error);
+    },
+    retry: false,
+  });
+
   return (
     <div className="relative">
-      <Button icon={<VscAdd />} onClick={openModal}>
+      <Button icon={<VscAdd />} onClick={openModal} disabled={isLoading}>
         Invite
       </Button>
 
@@ -29,7 +54,13 @@ const InviteMemberPopup = ({ projectName }: InviteMemberPopupProps) => {
         }
       >
         <p>Enter the email address of the member you want to invite.</p>
-        <Form layout="vertical" className="mt-4">
+        <Form
+          layout="vertical"
+          className="mt-4"
+          onFinish={(values) =>
+            mutate({ data: { email: values.email, projectId } })
+          }
+        >
           <Form.Item
             name="email"
             label={
@@ -50,7 +81,12 @@ const InviteMemberPopup = ({ projectName }: InviteMemberPopupProps) => {
           </Form.Item>
           <div className="flex items-center justify-end mt-4 space-x-4">
             <Button onClick={closeModal}>Cancel</Button>
-            <Button type="primary" htmlType="submit" className="ring-0">
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="ring-0"
+              loading={isLoading}
+            >
               Invite
             </Button>
           </div>
