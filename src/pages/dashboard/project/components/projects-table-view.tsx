611735b499b4a6335fdd527d2b8dbb9ddf3b317f
codemitrayt <codemitrayt@gmail.com>
2024-09-14 19:27:16 +0530

    add project table view

---
+++ src/pages/dashboard/project/components/projects-table-view.tsx
@@ -0,0 +1,114 @@
+import { Table, TableProps, Tag, Tooltip } from "antd";
+import { useNavigate } from "react-router-dom";
+import { CgEye } from "react-icons/cg";
+
+import UpdateProject from "../helpers/update";
+import DeleteProject from "../helpers/delete";
+
+import useUserInfo from "../../../../hooks/useUserInfo";
+import { Project } from "../../../../types";
+
+interface ProjectsTableViewProps {
+  projects: Project[];
+  refetchProjectList: () => void;
+}
+
+const columns: TableProps<Project>["columns"] = [
+  {
+    title: <span className="text-primary font-medium">Project Name</span>,
+    key: "projectName",
+    dataIndex: "name",
+  },
+  {
+    title: <span className="text-primary font-medium">Description</span>,
+    key: "description",
+    dataIndex: "description",
+  },
+  {
+    title: <span className="text-primary font-medium">Tags</span>,
+    key: "tags",
+    dataIndex: "tags",
+    render: (tags) => (
+      <div className="flex items-center">
+        {tags.map((tag: string) => (
+          <Tag color="#539BBB" key={tag} className="rounded-full px-3">
+            {tag}
+          </Tag>
+        ))}
+      </div>
+    ),
+  },
+  {
+    title: <span className="text-primary font-medium">Owner</span>,
+    key: "owner",
+    dataIndex: "user.fullName",
+    render: (_, { user }) => <span>{user.fullName}</span>,
+  },
+  {
+    title: <span className="text-primary font-medium">Role</span>,
+    key: "role",
+    dataIndex: "isAdmin",
+    render: (isAdmin) => (
+      <Tag color={isAdmin ? "orange" : "blue"}>
+        {isAdmin ? "Admin" : "Member"}
+      </Tag>
+    ),
+  },
+];
+
+const ProjectsTableView = ({
+  projects,
+  refetchProjectList,
+}: ProjectsTableViewProps) => {
+  const { user } = useUserInfo();
+  const navigate = useNavigate();
+  const handleRedirect = (projectId: string) => {
+    const url = `/dashboard/projects/${projectId}/details`;
+    navigate(url);
+  };
+
+  return (
+    <Table
+      bordered
+      pagination={false}
+      columns={[
+        ...columns,
+
+        {
+          title: <span className="text-primary font-medium">Actions</span>,
+          key: "actions",
+          render: (_, project) => (
+            <div className="flex items-center justify-center space-x-3">
+              <Tooltip title="View Project">
+                <button
+                  onClick={() => handleRedirect(project._id)}
+                  className="text-orange-500 hover:text-orange-500/80"
+                >
+                  <CgEye />
+                </button>
+              </Tooltip>
+
+              {user?._id === project.userId && (
+                <>
+                  <UpdateProject
+                    project={project}
+                    refetchProjectList={refetchProjectList}
+                  />
+
+                  <DeleteProject
+                    objectId={project._id}
+                    refetchProjectList={refetchProjectList}
+                  />
+                </>
+              )}
+            </div>
+          ),
+        },
+      ]}
+      rowKey="_id"
+      dataSource={projects}
+    />
+  );
+};
+
+export default ProjectsTableView;
