e73ee476b6c7e415d5ec4628615eae6b484e5160
codemitrayt <codemitrayt@gmail.com>
2024-09-16 07:46:31 +0530

    change ui

--- src/pages/dashboard/project/tasks/cards/project-task-card.tsx
+++ src/pages/dashboard/project/tasks/cards/project-task-card.tsx
@@ -1,8 +1,10 @@
 import { Avatar, Tag } from "antd";
+import dateformat from "dateformat";
 
 import { PiDotsThreeCircleLight } from "react-icons/pi";
 import { IoAttachOutline } from "react-icons/io5";
 import { LiaComment } from "react-icons/lia";
+import { IoMdTime } from "react-icons/io";
 
 import { ProjectTask } from "../../../../../types";
 import { capitalizeFirstLetter, cn } from "../../../../../utils";
@@ -13,23 +15,9 @@ interface ProjectTaskCardProps {
 }
 
 const PROJECT_TASK_PRIORITY_BG_COLOR = {
-  low: "green-inverse",
-  medium: "orange-inverse",
-  high: "red-inverse",
-};
-
-const PROJECT_TASK_STATUS_COLOR = {
-  todo: "red-inverse",
-  in_progress: "orange-inverse",
-  under_review: "blue-inverse",
-  completed: "green-inverse",
-};
-
-const STATUS_NAMING = {
-  todo: "TO DO",
-  in_progress: "IN PROGRESS",
-  under_review: "UNDER REVIEW",
-  completed: "COMPLETED",
+  low: "green",
+  medium: "orange",
+  high: "red",
 };
 
 const ProjectTaskCard = ({ projectTask }: ProjectTaskCardProps) => {
@@ -39,7 +27,7 @@ const ProjectTaskCard = ({ projectTask }: ProjectTaskCardProps) => {
         <div className="flex items-center justify-between">
           <div>
             {projectTask.tags.map((tag, ind) => (
-              <Tag key={ind} color="#2F667F" className="rounded-full px-3">
+              <Tag key={ind} className="rounded-full px-3">
                 {capitalizeFirstLetter(tag)}
               </Tag>
             ))}
@@ -50,28 +38,43 @@ const ProjectTaskCard = ({ projectTask }: ProjectTaskCardProps) => {
         </div>
         <h1 className="text-sm font-medium py-2">{projectTask.title}</h1>
         <p className="text-xs">{projectTask.description}</p>
-        <div className="flex items-center justify-between">
-          <div className="flex items-center mt-2">
-            <Tag color={PROJECT_TASK_STATUS_COLOR[projectTask.status]}>
-              {STATUS_NAMING[projectTask.status]}
-            </Tag>
+        <div className="flex items-center justify-between mt-3">
+          <div className="flex items-center">
             <Tag color={PROJECT_TASK_PRIORITY_BG_COLOR[projectTask.priority]}>
               {projectTask.priority.toUpperCase()}
             </Tag>
           </div>
-          <Avatar className="bg-primary">R</Avatar>
+          <div className="space-x-1">
+            <Avatar className="bg-primary !text-sm" size={25}>
+              R
+            </Avatar>
+            <span className="text-sm text-primary">Rushikesh Mungse</span>
+          </div>
         </div>
       </div>
 
       <div className="flex items-center justify-between p-3">
-        <Avatar.Group max={{ count: 2, style: { background: "#2F667F" } }}>
-          <Avatar className="bg-primary">R</Avatar>
-          <Avatar className="bg-primary">S</Avatar>
-          <Avatar className="bg-primary">T</Avatar>
-          <Avatar className="bg-primary">U</Avatar>
+        <Avatar.Group
+          className="!text-xs !border-primary"
+          max={{
+            count: 2,
+            style: { background: "#2F667F", border: "#2F667F" },
+          }}
+          size={25}
+        >
+          <Avatar className="bg-primary !text-xs !border-primary">R</Avatar>
+          <Avatar className="bg-primary !text-xs !border-primary">S</Avatar>
+          <Avatar className="bg-primary !text-xs !border-primary">T</Avatar>
+          <Avatar className="bg-primary !text-xs !border-primary">U</Avatar>
         </Avatar.Group>
 
         <div className="flex items-center justify-center space-x-3">
+          <div className="text-gray-800 flex items-center space-x-0.5 text-xs">
+            <IoMdTime />
+            <span className="text-xs">
+              {dateformat(projectTask.startDate, "dd/mm/yyyy")}
+            </span>
+          </div>
           <button className="text-primary hover:text-primary/80 transition-all flex items-center justify-center space-x-0.4">
             <IoAttachOutline className="size-4" />
             <span className="text-sm">12</span>
