a15d4bf2ce6ee6df3731055a56901d10261da48c
codemitrayt <codemitrayt@gmail.com>
2024-09-20 10:51:17 +0530

    Add assignee member card

---
+++ src/pages/dashboard/project/tasks/cards/member-card.tsx
@@ -0,0 +1,19 @@
+import { TbTrash } from "react-icons/tb";
+import { AssignUser } from "../../../../../types";
+
+interface MemberCard {
+  member: AssignUser;
+}
+
+const MemberCard = ({ member }: MemberCard) => {
+  return (
+    <div className="flex items-center justify-between border px-2 py-1 shadow-sm bg-secondary">
+      <span className="text-white">{member.memberEmailId}</span>
+      <button className="text-red-500 hover:text-red-500/80 transition-all">
+        <TbTrash />
+      </button>
+    </div>
+  );
+};
+
+export default MemberCard;
