ba258098216287eddb0a4c95a339007c0e860ad0
codemitrayt <codemitrayt@gmail.com>
2024-09-20 10:50:44 +0530

    Add condition to check error

--- src/hooks/useErrorHandler.ts
+++ src/hooks/useErrorHandler.ts
@@ -3,7 +3,7 @@ import { message } from "antd";
 const useErrorHandler = () => {
   const handleError = (error: any, msg = "Internal Server Error") =>
     message.open({
-      content: (error?.response?.data?.errors[0].msg as string) || msg,
+      content: error ? (error?.response?.data?.errors[0].msg as string) : msg,
       type: "error",
       className: "absolute top-[2px] right-[10px]",
       duration: 3,
