dacc5498f53475bc77d4b7be01a9b39574792ddb
codemitrayt <codemitrayt@gmail.com>
2024-09-20 12:45:17 +0530

    refactor

--- src/pages/dashboard/project/tasks/cards/member-card.tsx
+++ src/pages/dashboard/project/tasks/cards/member-card.tsx
@@ -1,4 +1,4 @@
-import { TbTrash } from "react-icons/tb";
+// import { TbTrash } from "react-icons/tb";
 import { AssignUser } from "../../../../../types";
 
 interface MemberCard {
@@ -9,9 +9,9 @@ const MemberCard = ({ member }: MemberCard) => {
   return (
     <div className="flex items-center justify-between border px-2 py-1 shadow-sm bg-secondary">
       <span className="text-white">{member.memberEmailId}</span>
-      <button className="text-red-500 hover:text-red-500/80 transition-all">
+      {/* <button className="text-red-500 hover:text-red-500/80 transition-all">
         <TbTrash />
-      </button>
+      </button> */}
     </div>
   );
 };
