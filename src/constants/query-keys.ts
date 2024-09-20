e28229798cd605a8ab2ecf387cd74be1dcef8288
codemitrayt <codemitrayt@gmail.com>
2024-09-20 10:50:26 +0530

    Add keys

---
+++ src/constants/query-keys.ts
@@ -0,0 +1,17 @@
+const queryKeys = {
+  auth: {},
+  project: {},
+  blSheet: {
+    get: "BL_SHEET_GET",
+    create: "BL_SHEET_CREATE",
+    update: "BL_SHEET_UPDATE",
+    delete: "BL_SHEET_DELETE",
+    getList: "BL_SHEET_LIST",
+  },
+  projectMember: {
+    getProjectMembers: "GET_PROJECT_MEMBERS",
+  },
+  todo: {},
+};
+
+export default queryKeys;
