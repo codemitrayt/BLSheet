75eb44bb1d3e66311f84ea382cd765f897d1eb47
codemitrayt <codemitrayt@gmail.com>
2024-09-20 10:51:56 +0530

    Add Assign User form

---
+++ src/pages/dashboard/project/tasks/forms/assign-user-form.tsx
@@ -0,0 +1,111 @@
+import { useEffect, useState } from "react";
+import { useMutation } from "react-query";
+import { useParams } from "react-router-dom";
+import { Select, Spin } from "antd";
+import { BiSearch } from "react-icons/bi";
+
+import queryKeys from "../../../../../constants/query-keys";
+import { AssignUser, ProjectMember, ProjectTask } from "../../../../../types";
+
+import { useDebounce } from "../../../../../hooks/useDebounce";
+import useUserInfo from "../../../../../hooks/useUserInfo";
+import useErrorHandler from "../../../../../hooks/useErrorHandler";
+import projectService from "../../../../../services/project-service";
+import MemberCard from "../cards/member-card";
+
+interface AssignUserForm {
+  projectTask: ProjectTask;
+}
+
+const AssignUserForm = ({}: AssignUserForm) => {
+  const { projectId } = useParams();
+  const { authToken } = useUserInfo();
+  const { handleError } = useErrorHandler();
+  const [values, setValues] = useState<AssignUser[]>([]);
+
+  const [options, setOptions] = useState([]);
+
+  const [localSearch, setLocalSearch] = useState<string>();
+  const debounceSearch = useDebounce(localSearch);
+
+  const handleAddUser = (info: { label: string; value: string }) => {
+    const existingUser = values.find(
+      (user) => user.memberEmailId === info.label
+    );
+
+    if (existingUser) {
+      handleError(null, "Already assigned this user.");
+      return;
+    }
+
+    setValues((prev) => [
+      ...prev,
+      { userId: info.value, memberEmailId: info.label },
+    ]);
+  };
+
+  useEffect(() => {
+    mutate({
+      params: { memberEmail: debounceSearch || "", status: "accepted" },
+    });
+  }, [debounceSearch]);
+
+  const { isLoading, mutate } = useMutation({
+    mutationKey: [queryKeys.projectMember.getProjectMembers],
+    mutationFn: ({
+      params,
+    }: {
+      params: { memberEmail: string; status: string };
+    }) =>
+      projectService().getProjectMembers({
+        data: { objectId: projectId },
+        authToken,
+        params,
+      }),
+    onSuccess: ({ data }) => {
+      const members = data?.message?.projectMembers || [];
+      const allMembers = members.map((member: ProjectMember) => {
+        return {
+          label: member.memberEmailId,
+          value: member._id,
+        };
+      });
+      setOptions(allMembers);
+    },
+    onError: (error) => {
+      console.error("ERROR :: project members ::", error);
+      handleError(error);
+    },
+    retry: false,
+  });
+
+  return (
+    <div className="p-6 rounded-lg bg-turnary border">
+      <div className="text-primary font-medium pl-1 pb-1">
+        Assign user to Project Task
+      </div>
+
+      <Select
+        showSearch
+        allowClear
+        value={values}
+        options={options}
+        onSearch={(value) => setLocalSearch(value)}
+        notFoundContent={isLoading ? <Spin size="small" /> : null}
+        onSelect={(_, info) => handleAddUser(info)}
+        filterOption={false}
+        suffixIcon={<BiSearch />}
+        placeholder="Search by member email"
+        className="w-full"
+      />
+
+      <div className="mt-3 space-y-2">
+        {values.map((value) => (
+          <MemberCard key={value.userId} member={value} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AssignUserForm;
