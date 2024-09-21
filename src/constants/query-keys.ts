fae521f749fb572520242e26a288755406d2eba9
codemitrayt <codemitrayt@gmail.com>
2024-09-21 20:33:49 +0530

    Add query key

--- src/constants/query-keys.ts
+++ src/constants/query-keys.ts
@@ -14,6 +14,8 @@ const queryKeys = {
   projectTask: {
     assignUserToProjectTask: "ASSIGN_USER_TO_PROJECT_TASK",
     removeAssignUserFromProjectTask: "REMOVE_ASSIGN_USER_TO_PROJECT_TASK",
+    createProjectTaskComment: "CREATE_PROJECT_TASK_COMMENT",
+    getProjectTaskComments: "GET_PROJECT_TASK_COMMENT",
   },
   todo: {},
 };
