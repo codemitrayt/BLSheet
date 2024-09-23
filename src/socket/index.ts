e86f52fbaff197092a042e665e67f1ebaaf47a19
codemitrayt <codemitrayt@gmail.com>
2024-09-23 23:08:37 +0530

    Add options

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
-const socket = io(import.meta.env.VITE_BACKEND_URL);
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
+  transports: ["websocket"],
+  secure: true,
+});
 
 socket.on("connect", () => {
   console.log("Connected to the server", socket.id);
