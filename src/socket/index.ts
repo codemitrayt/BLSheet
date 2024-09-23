295c030a0e0330726b001e7a7a3212093b1e0eb2
codemitrayt <codemitrayt@gmail.com>
2024-09-23 16:00:44 +0530

    fix url issue

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:5500");
+const socket = io(import.meta.env.VITE_BACKEND_URL);
 
 socket.on("connect", () => {
   console.log("Connected to the server", socket.id);
