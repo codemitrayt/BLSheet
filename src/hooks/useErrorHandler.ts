4be0f340ed000dcf35b0fb90b2f791fcfbf39e75
codemitrayt <codemitrayt@gmail.com>
2024-09-26 09:18:07 +0530

    Fixed

--- src/hooks/useErrorHandler.ts
+++ src/hooks/useErrorHandler.ts
@@ -2,7 +2,7 @@ import { message } from "antd";
 
 const useErrorHandler = () => {
   const handleError = (error: any, msg = "Internal Server Error") => {
-    const err = error ? (error?.response?.data?.errors[0].msg as string) : msg;
+    const err = error ? (error?.response?.data?.errors[0]?.msg as string) : msg;
     message.open({
       content: err ? err : msg,
       type: "error",
