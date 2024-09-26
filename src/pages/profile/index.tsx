cb434cfb6d0b93bf75473f278c61f095bfcd4df6
codemitrayt <codemitrayt@gmail.com>
2024-09-26 22:13:14 +0530

    make profile page responsive

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -5,18 +5,20 @@ const ProfilePage = () => {
   const { user } = useUserInfo();
   return (
     <div className="mx-auto container w-[90%]">
-      <div className="p-8 border rounded-lg shadow-sm w-full mt-6 bg-turnary">
-        <div className="flex items-center space-x-10">
+      <div className="p-3 md:p-8 border rounded-lg shadow-sm w-full mt-6 bg-turnary">
+        <div className="flex items-center space-x-3 md:space-x-10">
           <div>
-            <Avatar size={200} className="bg-primary">
-              {user?.fullName[0].toUpperCase()}
+            <Avatar className="bg-primary size-[50px] sm:size-[100px]  md:size-[200px]">
+              <span className="text-[20px] md:text-[40px]">
+                {user?.fullName[0].toUpperCase()}
+              </span>
             </Avatar>
           </div>
           <div className="flex flex-col space-y-6">
-            <h1 className="text-primary font-medium text-3xl">
+            <h1 className="text-primary font-medium text-xl md:text-3xl">
               {user?.fullName}
             </h1>
-            <div className="flex items-center space-x-8">
+            <div className="flex flex-col sm:flex-row sm:items-center space-y-4 sm:space-y-0 sm:space-x-8">
               <div className="flex flex-col">
                 <h1 className="text-primary font-medium text-sm">Role</h1>
                 <h1 className="text-gray-800 text-sm">{user?.role}</h1>
