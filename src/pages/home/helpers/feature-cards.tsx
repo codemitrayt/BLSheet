3cfb2cd8fca8f418ca6d61e4e06812ce781e73e1
codemitrayt <codemitrayt@gmail.com>
2024-10-03 18:52:40 +0530

    Add features

--- src/pages/home/helpers/feature-cards.tsx
+++ src/pages/home/helpers/feature-cards.tsx
@@ -3,8 +3,29 @@ import { TbReport } from "react-icons/tb";
 import { MdInsertChartOutlined } from "react-icons/md";
 
 import FeatureCard from "../cards/feature-card";
+import { BiCommentDots } from "react-icons/bi";
+import { RiTeamLine } from "react-icons/ri";
+import { VscIssues } from "react-icons/vsc";
 
 const features = [
+  {
+    title: "Collaboration and Communication",
+    description:
+      "Share your projects with team members, track progress, and collaborate on tasks efficiently.",
+    Icon: <BiCommentDots className="size-8 text-primary" />,
+  },
+  {
+    title: "Team and Resource Management",
+    description:
+      "Assign tasks to team members, track individual workloads, and manage resources to ensure balanced productivity.",
+    Icon: <RiTeamLine className="size-8 text-primary" />,
+  },
+  {
+    title: "Issue Tracking and Resolution",
+    description:
+      "Link issues to specific tasks, features, or milestones for a more integrated view of project challenges.",
+    Icon: <VscIssues className="size-8 text-primary" />,
+  },
   {
     title: "Productivity Tools",
     description:
