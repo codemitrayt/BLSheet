6235804f1a6cd77aaa81f20d155d95206d4c1e5c
codemitrayt <codemitrayt@gmail.com>
2024-10-03 23:43:15 +0530

    Fix grid color

--- src/components/effects/grid-effect.tsx
+++ src/components/effects/grid-effect.tsx
@@ -7,7 +7,7 @@ const GridEffect = ({
   return (
     <div
       className={cn(
-        "absolute inset-0 size-full bg-[size:14px_24px] bg-[linear-gradient(to_right,#ffffff0a_1px,transparent_1px),linear-gradient(to_bottom,#ffffff0a_1px,transparent_1px)]",
+        "absolute inset-0 size-full bg-[size:14px_24px] bg-[linear-gradient(to_right,#0000000a_1px,transparent_1px),linear-gradient(to_bottom,#0000000a_1px,transparent_1px)]",
         className
       )}
       {...props}
