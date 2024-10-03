f069e9efe9e474cb9bbc5883d686f8bd2d4d141d
codemitrayt <codemitrayt@gmail.com>
2024-10-03 19:15:00 +0530

    Add demo img comp

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,4 @@
+import DemoImg from "./helpers/demo-img";
 import FeatureCards from "./helpers/feature-cards";
 import Hero from "./helpers/hero";
 
@@ -5,6 +6,7 @@ const HomePage = () => {
   return (
     <div className="relative py-4">
       <Hero />
+      <DemoImg />
       <FeatureCards />
     </div>
   );
