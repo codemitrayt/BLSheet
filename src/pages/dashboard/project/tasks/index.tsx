e3c15f66908bea96b7d653509c6b619654d2b168
codemitrayt <codemitrayt@gmail.com>
2024-10-05 09:53:12 +0530

    Add project task filters

--- src/pages/dashboard/project/tasks/index.tsx
+++ src/pages/dashboard/project/tasks/index.tsx
@@ -3,35 +3,42 @@ import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-import { Project, ProjectTask } from "../../../../types";
 import ShowProjectTask from "./helpers/show";
 import CreateProjectTask from "./helpers/create";
+
 import useAuth from "../../../../hooks/useAuth";
 import useErrorHandler from "../../../../hooks/useErrorHandler";
-import projectTaskService from "../../../../services/project-task-service";
+import useProjectTaskFilters from "../../../../hooks/useProjectTaskFilters";
+
 import { useSocketProvider } from "../../../../providers/socket-provider";
+import { useProjectContext } from "../../../../providers/project-provider";
+
+import { ProjectTask } from "../../../../types";
+import ProjectTaskFilters from "../../../../components/filters/project-task-filters";
+
+import projectTaskService from "../../../../services/project-task-service";
 
 const ProjectTasks = () => {
   const socket = useSocketProvider();
   const { projectId } = useParams();
   const { authToken } = useAuth();
   const { handleError } = useErrorHandler();
+  const { project } = useProjectContext();
+  const { search, priority } = useProjectTaskFilters();
 
   const [projectTasks, setProjectTasks] = useState<ProjectTask[]>([]);
-  const [project, setProject] = useState<Project>();
 
   const { isLoading, refetch } = useQuery({
-    queryKey: ["GET_PROJECT_TASK", projectId],
+    queryKey: ["GET_PROJECT_TASK", projectId, search, priority],
     queryFn: () =>
       projectTaskService().getProjectTasks({
         data: { objectId: projectId },
         authToken,
+        params: { search, priority },
       }),
     onSuccess: ({ data }) => {
       const tasks = data?.message?.projectTasks || [];
-      const project = data?.message?.project || null;
       setProjectTasks(tasks);
-      setProject(project);
     },
     onError: (error) => {
       console.log("Error :: get project tasks ::", error);
@@ -52,27 +59,28 @@ const ProjectTasks = () => {
     };
   }, [socket]);
 
-  if (isLoading) {
-    return (
-      <div className="py-16 flex items-center justify-center">
-        <Spin />
-      </div>
-    );
-  }
-
   return (
     <div className="relative mt-3">
       <div className="flex items-center justify-between p-3 bg-gray-100 rounded-lg mb-4 border shadow-sm">
         <h1 className="text-primary font-bold">{project?.name}</h1>
-        <CreateProjectTask refetchProjectTaskList={refetch} />
+        <div className="flex items-center justify-center space-x-4">
+          <ProjectTaskFilters />
+          <CreateProjectTask refetchProjectTaskList={refetch} />
+        </div>
       </div>
 
-      <div className="w-[calc(100vw_-100px)] lg:w-[calc(100vw_-290px)] border p-6 rounded-xl shadow-sm">
-        <ShowProjectTask
-          projectTasks={projectTasks}
-          refetchProjectTask={refetch}
-        />
-      </div>
+      {isLoading && !projectTasks.length ? (
+        <div className="py-16 flex items-center justify-center">
+          <Spin />
+        </div>
+      ) : (
+        <div className="w-[calc(100vw_-100px)] lg:w-[calc(100vw_-290px)] border p-6 rounded-xl shadow-sm">
+          <ShowProjectTask
+            projectTasks={projectTasks}
+            refetchProjectTask={refetch}
+          />
+        </div>
+      )}
     </div>
   );
 };
