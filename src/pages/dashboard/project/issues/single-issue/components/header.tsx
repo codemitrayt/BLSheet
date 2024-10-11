2ea4d11414ec249be8c0075385fcbfd6980f6fc6
codemitrayt <codemitrayt@gmail.com>
2024-10-11 09:09:55 +0530

    Build issue header UI

---
+++ src/pages/dashboard/project/issues/single-issue/components/header.tsx
@@ -0,0 +1,46 @@
+import { VscIssues } from "react-icons/vsc";
+import dateFormat from "dateformat";
+
+import { Issue } from "../../../../../../types";
+import { capitalizeFirstLetter } from "../../../../../../utils";
+
+interface IssueHeader {
+  issue: Issue;
+}
+
+const IssueHeader = ({ issue }: IssueHeader) => {
+  return (
+    <div className="border-b pb-2 border-primary">
+      <div className="flex items-center justify-between">
+        <h1 className="text-primary font-medium">{issue?.title}</h1>
+      </div>
+
+      <div className="flex items-center space-y-2 mt-1">
+        <div className="md:flex items-center justify-center space-x-2">
+          <div className="flex items-center space-x-2">
+            <div className="flex items-center justify-center space-x-1 border rounded-full px-4 py-1 bg-emerald-700 text-white">
+              <VscIssues />
+              <span>{capitalizeFirstLetter(issue?.status)}</span>
+            </div>
+            <span className="text-sm text-primary font-medium">
+              {issue.author.fullName}
+            </span>
+          </div>
+
+          <div className="sm:flex items-center justify-center">
+            <div className="text-sm">
+              opened this issue on {dateFormat(issue.createdAt, "dd/mm/yyyy")}
+            </div>
+            {!!issue.commentCount && (
+              <div className="text-primary text-sm">
+                ・{issue.commentCount} comments
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IssueHeader;
