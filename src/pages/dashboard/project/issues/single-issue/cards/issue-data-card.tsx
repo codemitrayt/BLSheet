ac74bd668635bc98a873deb1a0323c7189a61071
codemitrayt <codemitrayt@gmail.com>
2024-10-11 09:09:19 +0530

    Add issue card data

---
+++ src/pages/dashboard/project/issues/single-issue/cards/issue-data-card.tsx
@@ -0,0 +1,52 @@
+import { Avatar, Tag } from "antd";
+
+import { Issue, IssueStatus } from "../../../../../../types";
+import { useProjectContext } from "../../../../../../providers/project-provider";
+
+interface IssueDataCards {
+  issue: Issue;
+}
+
+const IssueDataCard = ({ issue }: IssueDataCards) => {
+  const { project } = useProjectContext();
+
+  return (
+    <div className="space-y-3 w-full col-span-2">
+      <div className="space-y-2 p-3 rounded-lg bg-gray-100 border border-gray-200 shadow-sm h-fit w-full">
+        <h1 className="text-primary text-sm font-medium">Assingees</h1>
+        <div className="flex items-center">
+          <Avatar className="bg-primary">R</Avatar>
+        </div>
+      </div>
+
+      <div className="space-y-2 p-3 rounded-lg bg-gray-100 border border-gray-200 shadow-sm h-fit w-full">
+        <h1 className="text-primary text-sm font-medium">Labels</h1>
+        <div className="flex items-center">
+          {issue.labels.map((label, i) => (
+            <Tag color="orange" key={i}>
+              {label}
+            </Tag>
+          ))}
+        </div>
+      </div>
+
+      <div className="space-y-2 p-3 rounded-lg bg-gray-100 border border-gray-200 shadow-sm h-fit w-full">
+        <h1 className="text-primary text-sm font-medium">Project</h1>
+        <div className="flex items-center">
+          <h1 className="text-sm">{project?.name}</h1>
+        </div>
+      </div>
+
+      <div className="space-y-2 p-3 rounded-lg bg-gray-100 border border-gray-200 shadow-sm h-fit w-full">
+        <h1 className="text-primary text-sm font-medium">Status</h1>
+        <div className="flex items-center">
+          <Tag color={issue.status === IssueStatus.OPEN ? "green" : "red"}>
+            {issue.status.toUpperCase()}
+          </Tag>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IssueDataCard;
