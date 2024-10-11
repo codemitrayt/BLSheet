a9bdb38990e94f8ed7aca65e00a14a47ee17ff40
codemitrayt <codemitrayt@gmail.com>
2024-10-11 10:32:57 +0530

    Add on click event  listener on project card

--- src/pages/dashboard/project/cards/project-card.tsx
+++ src/pages/dashboard/project/cards/project-card.tsx
@@ -22,61 +22,65 @@ const ProjectCard = ({ project, refetchProjectList }: ProjectCardProps) => {
 
   return (
     <div className="border h-full shadow-sm rounded-lg overflow-hidden">
-      <img
-        src={project.img}
-        alt={project.name}
-        className="w-full h-48 object-cover"
-      />
+      <div className="cursor-pointer" onClick={handleRedirect}>
+        <img
+          src={project.img}
+          alt={project.name}
+          className="w-full h-48 object-cover"
+        />
 
-      <div className="p-3">
-        <div className="flex items-center space-x-2">
-          <Badge color="#2F667F" status="processing" />
-          <h1 className="text-primary font-medium">{project.name}</h1>
-        </div>
-        <div className="py-2">
-          {project.tags.map((tag) => (
-            <Tag color="#539BBB" key={tag} className="rounded-full px-3">
-              {capitalizeFirstLetter(tag)}
-            </Tag>
-          ))}
-        </div>
-
-        <p className="text-sm text-gray-600 h-[50px] hidden md:block">
-          {strSlice(project.description, 100)}
-        </p>
-
-        <p className="text-sm text-gray-600 md:hidden">{project.description}</p>
-
-        <div className="flex items-center justify-between mt-3">
-          <div>
-            <Tag color={project.isAdmin ? "orange" : "blue"}>
-              {project.isAdmin ? "Admin" : "Member"}
-            </Tag>
+        <div className="p-3">
+          <div className="flex items-center space-x-2">
+            <Badge color="#2F667F" status="processing" />
+            <h1 className="text-primary font-medium">{project.name}</h1>
           </div>
-          <div className="flex items-center justify-center space-x-3">
-            <Tooltip title="View Project">
-              <button
-                onClick={handleRedirect}
-                className="text-orange-500 hover:text-orange-500/80"
-              >
-                <CgEye />
-              </button>
-            </Tooltip>
-
-            {project.isAdmin && (
-              <>
-                <UpdateProject
-                  project={project}
-                  refetchProjectList={refetchProjectList}
-                />
-
-                <DeleteProject
-                  objectId={project._id}
-                  refetchProjectList={refetchProjectList}
-                />
-              </>
-            )}
+          <div className="py-2">
+            {project.tags.map((tag) => (
+              <Tag color="#539BBB" key={tag} className="rounded-full px-3">
+                {capitalizeFirstLetter(tag)}
+              </Tag>
+            ))}
           </div>
+
+          <p className="text-sm text-gray-600 h-[50px] hidden md:block">
+            {strSlice(project.description, 100)}
+          </p>
+
+          <p className="text-sm text-gray-600 md:hidden">
+            {project.description}
+          </p>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-between px-3 py-2 mt-2 border-t">
+        <div>
+          <Tag color={project.isAdmin ? "orange" : "blue"}>
+            {project.isAdmin ? "Admin" : "Member"}
+          </Tag>
+        </div>
+        <div className="flex items-center justify-center space-x-3">
+          <Tooltip title="View Project">
+            <button
+              onClick={handleRedirect}
+              className="text-orange-500 hover:text-orange-500/80"
+            >
+              <CgEye />
+            </button>
+          </Tooltip>
+
+          {project.isAdmin && (
+            <>
+              <UpdateProject
+                project={project}
+                refetchProjectList={refetchProjectList}
+              />
+
+              <DeleteProject
+                objectId={project._id}
+                refetchProjectList={refetchProjectList}
+              />
+            </>
+          )}
         </div>
       </div>
     </div>
