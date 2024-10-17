8f07af792ed36f22d5953fb48b24701212188495
codemitrayt <codemitrayt@gmail.com>
2024-10-18 00:43:41 +0530

    Add analytics cards

---
+++ src/pages/dashboard/project/single-project/cards/analytics-card.tsx
@@ -0,0 +1,45 @@
+import { IoIosCheckmarkCircle } from "react-icons/io";
+import { RiTodoFill } from "react-icons/ri";
+import { MdAssignmentInd } from "react-icons/md";
+import { MdReviews } from "react-icons/md";
+
+import { cn } from "../../../../../utils";
+import { ProjectTaskStatus } from "../../../../../types";
+
+const IconMap = {
+  todo: <RiTodoFill className="size-4 xl:size-6" />,
+  in_progress: <MdAssignmentInd className="size-4 xl:size-6" />,
+  completed: <IoIosCheckmarkCircle className="size-4 xl:size-6" />,
+  under_review: <MdReviews className="size-4 xl:size-6" />,
+};
+
+const AnalyticsCard = ({
+  title,
+  status,
+  count = 0,
+}: {
+  isLast?: boolean;
+  title: string;
+  status: ProjectTaskStatus;
+  count?: number;
+}) => {
+  return (
+    <div
+      className={cn(
+        "xl:h-[100px] flex text-gray-600 border rounded-md shadow-sm"
+      )}
+    >
+      <div className="w-full p-2">
+        <div className="flex items-center justify-between">
+          <h1 className="font-medium text-sm xl:text-md">{title}</h1>
+          {IconMap[status]}
+        </div>
+        <div className="flex items-center justify-center">
+          <h1 className="font-medium p-2 text-sm xl:text-3xl">{count}</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AnalyticsCard;
