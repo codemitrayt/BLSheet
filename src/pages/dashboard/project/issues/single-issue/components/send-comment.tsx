dd43b5fb21e49c1b75c69df264fa052666ceaa34
codemitrayt <codemitrayt@gmail.com>
2024-10-19 18:25:35 +0530

    refactor

--- src/pages/dashboard/project/issues/single-issue/components/send-comment.tsx
+++ src/pages/dashboard/project/issues/single-issue/components/send-comment.tsx
@@ -1,18 +1,11 @@
-import { Button } from "antd";
-import { FaCheckCircle } from "react-icons/fa";
 import ReactQuill from "react-quill";
 
 const SendComment = () => {
   return (
     <div className="mt-3">
       <ReactQuill />
-
-      <div className="flex items-center justify-end mt-3 space-x-4">
-        <Button danger type="primary" icon={<FaCheckCircle />}>
-          Close issue
-        </Button>
-
-        <button className="ring-0 bg-emerald-600 text-white hover:bg-emerald-600/70 rounded-md px-3 py-1">
+      <div className="flex items-center justify-end mt-2">
+        <button className="ring-0 bg-emerald-600 text-white hover:bg-emerald-600/70 rounded-md px-3 py-1 w-fit">
           Comment
         </button>
       </div>
