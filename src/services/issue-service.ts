c17592929feb83e128e74b06c57e8910c35f3823
codemitrayt <codemitrayt@gmail.com>
2024-10-20 18:01:39 +0530

    Add issue service

--- src/services/issue-service.ts
+++ src/services/issue-service.ts
@@ -36,6 +36,89 @@ const issueService = {
       url: urls.issue.changeIssueStatus,
       authToken,
     }),
+
+  updateIssue: ({ authToken, params, data }: RequestType) =>
+    request({
+      method: "PUT",
+      params,
+      data,
+      url: urls.issue.updateIssue,
+      authToken,
+    }),
+
+  deleteIssue: ({ authToken, params }: RequestType) =>
+    request({
+      method: "DELETE",
+      params,
+      url: urls.issue.deleteIssue,
+      authToken,
+    }),
+
+  assignUserToIssue: ({ authToken, params, data }: RequestType) =>
+    request({
+      method: "POST",
+      params,
+      data,
+      url: urls.issue.assignUserToIssue,
+      authToken,
+    }),
+
+  removeAssignedUserFormIssue: ({ authToken, params, data }: RequestType) =>
+    request({
+      method: "POST",
+      params,
+      data,
+      url: urls.issue.removeAssignedUserFormIssue,
+      authToken,
+    }),
+
+  createIssueComment: ({ authToken, data }: RequestType) =>
+    request({
+      method: "POST",
+      data,
+      url: urls.issue.createIssueComment,
+      authToken,
+    }),
+
+  updateIssueComment: ({ authToken, data }: RequestType) =>
+    request({
+      method: "PUT",
+      data,
+      url: urls.issue.updateIssueComment,
+      authToken,
+    }),
+
+  getIssueComments: ({ authToken, data }: RequestType) =>
+    request({
+      method: "POST",
+      data,
+      url: urls.issue.getIssueComments,
+      authToken,
+    }),
+
+  replyToIssueComment: ({ authToken, data }: RequestType) =>
+    request({
+      method: "POST",
+      data,
+      url: urls.issue.replyToIssueComment,
+      authToken,
+    }),
+
+  getIssueCommentReplies: ({ authToken, data }: RequestType) =>
+    request({
+      method: "POST",
+      data,
+      url: urls.issue.getIssueCommentReplies,
+      authToken,
+    }),
+
+  deleteIssueComment: ({ authToken, data }: RequestType) =>
+    request({
+      method: "DELETE",
+      data,
+      url: urls.issue.deleteIssueComment,
+      authToken,
+    }),
 };
 
 export default issueService;
